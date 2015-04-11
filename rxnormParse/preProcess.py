f7cf273e378deaa8190955717fc2a2c9af858651
Marcus Naymik <mnaymik@asu.edu>
2015-04-11 16:37:28 -0700

    Added preProcessing.py 1.0

---
+++ rxnormParse/preProcess.py
@@ -0,0 +1,42 @@
+import requests
+
+vmr1 = open('testvMRout.txt', 'r')
+vmr2 = open('testvMRout2.txt','r')
+vmrLines1 = vmr1.read().splitlines()
+vmrLines2 = vmr2.read().splitlines()
+vmr1.close()
+vmr2.close()
+#print vmrLines1
+out = open('TEST.txt','w')
+
+def getName(idCode,idType,DATE):
+	#print idCode+' '+idType
+	rxjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype='+idType+'&id='+idCode)
+	rxcuiString = rxjson.json()['idGroup']['rxnormId'][0]
+	propertyjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui/'+rxcuiString+'/properties.json')
+	rxNameString = propertyjson.json()['properties']['name']
+	out.write(rxNameString+' 0 MG injectable;'+DATE+'\n')
+	
+
+
+for line in vmrLines1:
+	#print line
+	idCode = line.split('\t')[0]
+		#print '\n'+idCode
+	idType = line.split('\t')[1]
+		#print '\n'+idType
+	DATE = line.split('\t')[2]	
+	getName(idCode,idType,DATE)
+
+out.write('******\n')
+
+for line in vmrLines2:
+	#print line
+	idCode = line.split('\t')[0]
+		#print '\n'+idCode
+	idType = line.split('\t')[1]
+		#print '\n'+idType
+	DATE = line.split('\t')[2]	
+	getName(idCode,idType,DATE)
+
+out.close()
