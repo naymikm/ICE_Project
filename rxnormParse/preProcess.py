28d2233d007dd3645142da89ec9a8a7385941cb1
Marcus Naymik <mnaymik@asu.edu>
2015-04-25 11:31:53 -0700

    Interactive user

--- rxnormParse/preProcess.py
+++ rxnormParse/preProcess.py
@@ -2,12 +2,15 @@ import requests
 
 vmr1 = open('testvMRout.txt', 'r')		
 vmr2 = open('testvMRout2.txt','r')
+user = open('userOut.txt','r')
 vmrLines1 = vmr1.read().splitlines()	
 vmrLines2 = vmr2.read().splitlines()
+userLines = user.read().splitlines()
 vmr1.close()
 vmr2.close()							
 #print vmrLines1
-out = open('TEST.txt','w')				
+out = open('TEST.txt','w')
+out2 = open('userTest.txt','w')				
 
 # def searchName(userName) in search.py ....
 
@@ -31,7 +34,26 @@ def getName(idCode,idType,DATE):
 			out.write(rxNameString+' injectable;'+DATE+'\n')
 		else:
 			out.write(rxNameString+' 0 MG injectable;'+DATE+'\n')
-	
+
+def getName2(idCode,idType,DATE):
+	#print idCode+' '+idType
+	if idType != 'RXCUI':
+		rxjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui.json?idtype='+idType+'&id='+idCode)				
+		rxcuiString = rxjson.json()['idGroup']['rxnormId'][0]														
+		propertyjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui/'+rxcuiString+'/properties.json')		
+		rxNameString = propertyjson.json()['properties']['name']
+		if 'MG' in rxNameString.split(' '):
+			out2.write(rxNameString+' injectable;'+DATE+'\n')
+		else:
+			out2.write(rxNameString+' 0 MG injectable;'+DATE+'\n')
+	else:
+		propertyjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui/'+idCode+'/properties.json')		
+		rxNameString = propertyjson.json()['properties']['name']
+		if 'MG' in rxNameString.split(' '):
+			out2.write(rxNameString+' injectable;'+DATE+'\n')
+		else:
+			out2.write(rxNameString+' 0 MG injectable;'+DATE+'\n')
+
 
 
 for line in vmrLines1:						
@@ -55,4 +77,14 @@ for line in vmrLines2:
 	getName(idCode,idType,DATE)
 
 out.write('******')
+
+for line in userLines:
+	#print line
+	idCode = line.split('\t')[0]						
+		#print '\n'+idCode
+	idType = line.split('\t')[1]
+		#print '\n'+idType
+	DATE = line.split('\t')[2]	
+	getName2(idCode,idType,DATE)
+
 out.close()												
--- rxnormParse/search.py
+++ rxnormParse/search.py
@@ -4,26 +4,58 @@ f = open('stest.txt','r')
 flines = f.read().splitlines()
 f.close()
 out = open('userOut.txt','w')
+out2 = open('noTerms.txt','w')
 
 
-def searchName(userName,DATE):
+#add a loop for approximate terms
+
+def getInput(slist):
+	inpt = int(raw_input("Enter the number of the correct term:	"))
+	if inpt not in range(0,len(slist)):
+		print 'Please enter a number between 0 and '+str(len(slist)-1)
+		return getInput(slist)
+	else:
+		print '\nYou chose '+str(slist[inpt])
+		choose = raw_input('Is this correct? [Y/N]') 
+		if choose.upper() not in ['Y','YES']:
+			return getInput(slist)
+		else:
+			return inpt
+
+def suggestName(userName,DATE):
 	suggest = requests.get('http://rxnav.nlm.nih.gov/REST/spellingsuggestions.json?name='+userName)
 	if (suggest.json()['suggestionGroup']['suggestionList'] is None):
-		Error = '\nThe term "'+userName+'" has no search result. Please try another term\nor continue without it.\n'
+		Error = '\nThe term "'+userName+'" had no search result. Will attempt to use raw input.\n'
 		print Error
-		return
+		out2.write(userName+'\t'+DATE+'\n')
+		return userName
 	else:
-		suggString = suggest.json()['suggestionGroup']['suggestionList']['suggestion'][0]
-#		print suggString+'\n'
+		print 'You listed\t'+userName+'\n'
+		suggestList = suggest.json()['suggestionGroup']['suggestionList']['suggestion']
+		i = 0
+		while i < len(suggestList):
+			print str(i)+'\t.....\t'+str(suggestList[i])+'\n'
+			i+=1
+		return suggestList[getInput(suggestList)]
+		print '----------------------------------'
+
+
+def searchName(sName,DATE):
+		suggString = suggestName(sName,DATE)
 		search = requests.get('http://rxnav.nlm.nih.gov/REST/approximateTerm.json?term='+suggString)
-		rxcuiString = search.json()['approximateGroup']['candidate'][0]['rxcui']
-		propertyjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui/'+rxcuiString+'/properties.json')
-		if propertyjson.json() is None:
-			Error2 = '\nThe term "'+userName+'" does not have a valid RxCUI. Please try another term\nor continue without it.\n'
-			print 	Error2
-			return
-		else:	
-			out.write(rxcuiString+'\t'+'RXCUI'+'\t'+DATE+'\n')
+		searchList = search.json()['approximateGroup']['candidate']
+		i = 0
+		while i < len(searchList):
+			rxcuiString = searchList[i]['rxcui']
+			propertyjson = requests.get('http://rxnav.nlm.nih.gov/REST/rxcui/'+rxcuiString+'/properties.json')
+			if propertyjson.json() is None:
+				i+=1
+				continue
+			else:
+				print 'Found RxCUI: '+rxcuiString+' for the item '+'"'+suggString+'" moving to next item...' 
+				print '\n-----------------------------------\n'
+				out.write(rxcuiString+'\t'+'RXCUI'+'\t'+DATE+'\n')
+				break
 
 
 for line in flines:
@@ -31,4 +63,6 @@ for line in flines:
 	userInputDate = line.split('\t')[1]
 	searchName(userInputName,userInputDate)
 
+
 out.close()
+out2.close()
